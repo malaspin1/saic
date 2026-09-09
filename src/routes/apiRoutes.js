78b4a77ba5979bf915c7d25398c4b7cb857aea1d
of13095ocmj@gmail.com <of13095ocmj@gmail.com>
2026-09-09 16:15:59 -0300

    API pronta

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
 const router = express.Router();
-router.get('/teste',(req,res)=>res.send({message:"api esta no ar"}))
+router.get('/sprint',(req,res)=>res.send({message:"api esta no ar"}))
 
 export default router
